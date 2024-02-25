8364fc0a80687d1210b9064b025190ee49523791
Grigory <weissman1979@gmail.com>
2024-02-25 17:03:55 +0400

    init repo

--- src/js/game.js
+++ src/js/game.js
@@ -1,14 +1,16 @@
-class Game {
+import { Character } from "./domain.js"
+
+export default class Game {
     start() {
-      console.log('game started');
+        console.log('game started');
     }
-  }
-  
-  class GameSavingData {
-  }
-  
-  function readGameSaving() {
-  }
-  
-  function writeGameSaving() {
-  }
+}
+
+export class GameSavingData {
+}
+
+export function readGameSaving() {
+}
+
+export function writeGameSaving() {
+}
